7b38ab033f3606afe9c61a8f8f0fd290379d3802
sureshsivatatikonda <142252214+sureshsivatatikonda@users.noreply.github.com>
2023-12-18 21:33:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,18 +20,18 @@ app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 
-// if (process.env.NODE_ENV === 'development') {
-//   const __dirname = path.resolve();
-//   app.use(express.static(path.join(__dirname, '/frontend/dist')));
+if (process.env.NODE_ENV === 'development') {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-//   );
-// } else {
-//   app.get('/', (req, res) => {
-//     res.send('API is running....');
-//   });
-// }
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);

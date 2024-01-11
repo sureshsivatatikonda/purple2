33cc33f5a7a78c21517fe4f1a1c473e02a3ca9c3
sureshsivatatikonda <142252214+sureshsivatatikonda@users.noreply.github.com>
2024-01-11 15:00:05 +0530

    Update 5135

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(
   cors({
     // origin:"https://symphonious-piroshki-f63932.netlify.app",
-    origin:"https://purple.tekmagnus.com",
+    origin:"http://localhost:5173",
     methods:["GET","POST","PUT","DELETE"]
   })
 )
